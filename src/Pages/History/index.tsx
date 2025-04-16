57e7e30695bb874bd03f5af07006c6d2364877cb
ooliveiratg <menezesth16@gmail.com>
2025-04-15 22:59:55 -0300

    I added the history

--- src/Pages/History/index.tsx
+++ src/Pages/History/index.tsx
@@ -1,3 +1,48 @@
+import { HistoryContainer, HistoryList } from "./styled";
+
 export function History() {
-    return <h1>history</h1>
+  return (
+    <HistoryContainer>
+      <h1>history</h1>
+      <HistoryList>
+        <table>
+            <thead>
+                <tr>
+                    <th>Tarefa</th>
+                    <th>Duração</th>
+                    <th>Inicio</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tr>
+                <td>tarefa</td>
+                <td>20 minutos</td>
+                <td>há 2 meses</td>
+                <td>concluido</td>
+            </tr>
+            <tr>
+                <td>tarefa</td>
+                <td>20 minutos</td>
+                <td>há 2 meses</td>
+                <td>concluido</td>
+
+            </tr>
+            <tr>
+                <td>tarefa</td>
+                <td>20 minutos</td>
+                <td>há 2 meses</td>
+                <td>concluido</td>
+
+            </tr>
+            <tr>
+                <td>tarefa</td>
+                <td>20 minutos</td>
+                <td>há 2 meses</td>
+                <td>concluido</td>
+
+            </tr>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
