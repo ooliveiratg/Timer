676d5eff41e824f6126ee06234028811cf03fc4a
ooliveiratg <menezesth16@gmail.com>
2025-06-25 23:43:57 -0300

    fix(pages): normalize casing of Pages folder to avoid Linux conflicts

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
-import { Home } from "./pages/Home";
-import { History } from "./pages/History";
+import { Home } from "./pages/home";
+import { History } from "./pages/history";
 
 export function Router() {
   return (
